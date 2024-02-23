9318721e88c9e82e0a982f121d44bb85c5b19a48
Boryana Baeva <borenceto@gmail.com>
2024-02-23 10:20:46 +0100

    redo of the structure of channel module

--- src/app/channels/channels-routing.module.ts
+++ src/app/channels/channels-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from "@angular/router";
+import {ViewChannelComponent} from "./pages/view-channel/view-channel.component";
+import {AddChannelComponent} from "./pages/add-channel/add-channel.component";
+import {EditChannelComponent} from "./pages/edit-channel/edit-channel.component";
 
+const routes: Routes = [
+  {path: ':id', component: ViewChannelComponent},
+  {path: 'add', component: AddChannelComponent},
+  {path: 'edit/:id', component: EditChannelComponent},
+];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class ChannelsRoutingModule { }
