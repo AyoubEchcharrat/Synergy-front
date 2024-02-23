eab2eaab413c8352d804609a2e4882c19fa9590e
Voolak <abarzana.perso@gmail.com>
2024-02-23 16:30:27 +0100

    List Users Sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterLink } from '@angular/router';
 import { ChannelsModule } from '../channels/channels.module';
+import { UsersModule } from '../users/users.module';
 
 @NgModule({
   declarations: [SidebarComponent, HeaderComponent],
-  imports: [CommonModule, RouterLink, ChannelsModule],
+  imports: [CommonModule, RouterLink, ChannelsModule, UsersModule],
   exports: [SidebarComponent, HeaderComponent],
 })
 export class CoreModule {}
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListUsersComponent } from './pages/list-users/list-users.component';
-
-
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    ListUsersComponent
-  ],
-  exports: [
-    ListUsersComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ListUsersComponent],
+  imports: [CommonModule, RouterLink],
+  exports: [ListUsersComponent],
 })
-export class UsersModule { }
+export class UsersModule {}
