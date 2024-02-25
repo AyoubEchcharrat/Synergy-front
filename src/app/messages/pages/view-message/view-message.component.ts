b1df8d17a26ab8e76b3a022443a484a0af1ef6c6
AyoubEchcharrat <ayoub.ech-charrat@hotmail.fr>
2024-02-25 17:46:09 +0100

    add style of a message

--- src/app/messages/pages/view-message/view-message.component.ts
+++ src/app/messages/pages/view-message/view-message.component.ts
@@ -1,25 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {PublicMessage} from "../../../core/models/public-messages";
-import {ViewMessage} from "../../../core/models/view-message";
+import { Component, Input, OnInit } from '@angular/core';
+import { ViewMessage } from '../../../core/models/view-message';
 
 @Component({
   selector: 'app-view-message',
   templateUrl: './view-message.component.html',
-  styleUrl: './view-message.component.css'
+  styleUrl: './view-message.component.css',
 })
-export class ViewMessageComponent implements OnInit{
+export class ViewMessageComponent implements OnInit {
   @Input() message!: ViewMessage;
 
   ngOnInit(): void {
     console.log(this.message);
   }
 
+  toggleUpvote() {}
 
-  toggleUpvote() {
-
-  }
-
-  toggleDownvote() {
-
-  }
+  toggleDownvote() {}
 }
